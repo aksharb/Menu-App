85a44f3fd3b8e2d40b743968f8dbd5647d0567fe
Akshar Bisht <bisht.akshar@gmail.com>
2025-03-16 11:59:10 +0530

    Added MenuItem file

---
+++ Menu App/MenuItem.swift
@@ -0,0 +1,14 @@
+//
+//  MenuItem.swift
+//  Menu App
+//
+//  Created by Akshar Bisht on 16/03/25.
+//
+
+import Foundation
+
+struct MenuItem {
+    var name: String
+    var price: String
+    var imageName: String
+}
