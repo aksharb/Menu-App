ee2669d33d42a618949e6a79ff333cf89e653d2b
Akshar Bisht <bisht.akshar@gmail.com>
2025-03-16 08:47:09 +0530

    Initial Commit

---
+++ Menu App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Menu App
+//
+//  Created by Akshar Bisht on 16/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Menu App/Menu_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Menu_AppApp.swift
+//  Menu App
+//
+//  Created by Akshar Bisht on 16/03/25.
+//
+
+import SwiftUI
+
+@main
+struct Menu_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
